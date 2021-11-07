456bc8f578b2c1fff8a11be227299238bd0bab0c
Alejandro Berrio Marin <alejo86a@gmail.com>
2021-11-07 15:27:43 -0500

    call memoize but retrieve a function

--- index.js
+++ index.js
@@ -1,49 +1,43 @@
 const results = {};
-const memoize = (func) => {
+// stores data (value) by key
+async function cache_store(key, value) {
+    results[key] = value;
+}
+// retrieves data by key (if it exists)
+async function cache_retrieve(key) {
+    return results[key];
+}
+function memoize(func) {
     return (...args) => {
       const argsKey = JSON.stringify(args);
-      if (!results[argsKey]) {
-        results[argsKey] = func(...args);
-      }
-      return results[argsKey];
+      console.log('entre aqui')
+      Promise.race([cache_retrieve(argsKey) , func]).then((value) => {
+        cache_store(argsKey, value)
+        console.log('value', value)
+        return value;
+      });
     };
   };
 
-const getAllRooms = memoize((total) =>{
+  async function slow_function(input){
+    console.log('entre aca')
     const allRooms = [];
-    for(let i =1; i<=total;i++){
+    for(let i =1; i<=input;i++){
         allRooms.push({
             name: 'Room #'+i
         })
     }
     return allRooms;
-})
-
-// stores data (value) by key
-async function cache_store(key, value) {
-}
-// retrieves data by key (if it exists)
-async function cache_retrieve(key) {
-}
-// fetches data from a slow data source
-async function slow_function(input) {
-    return getAllRooms(input);
-}
-
-// runs faster than slow_function by using cache functions
-// function memoize(slow_function) {
-//     return fast_function;
-// }
+};
 
 
-  
-const fast_function = (num) => memoize(slow_function(num));
+console.time("First call");
+console.log(memoize(slow_function(100)))
+console.timeEnd("First call");
 
 
-  console.time("First call");
-  console.log(getAllRooms(100));
-  console.timeEnd("First call");
 
-  console.time("Second call");
-  console.log(getAllRooms(100));
-  console.timeEnd("Second call");
+
+console.time("Second call");
+console.log(memoize(slow_function(100)));
+console.timeEnd("Second call");
